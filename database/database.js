a59356ef692121e32f2f8511991da8c6cce8a6e2
Piyush <sainipiyush8860@gmail.com>
2023-11-16 21:20:55 +0530

    mongodb error

--- database/database.js
+++ database/database.js
@@ -1,7 +1,7 @@
 const Mongoose = require('mongoose')
 
 const connectDB = () => {   
-    Mongoose.connect(process.env.MONGO_URI).then(()=>{
+    Mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true }).then(()=>{
         console.log("Database has been created")
     })
     
