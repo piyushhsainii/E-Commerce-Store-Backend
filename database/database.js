146882c184f01b62c2a9f8e3ffdecf53f7e9f2d4
Piyush <sainipiyush8860@gmail.com>
2023-11-16 21:24:17 +0530

    mongodb error2

--- database/database.js
+++ database/database.js
@@ -4,6 +4,9 @@ const connectDB = () => {
     Mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true }).then(()=>{
         console.log("Database has been created")
     })
+    .catch((error) => {
+        console.error("Error connecting to the database:", error);
+      });
     
 }
 
